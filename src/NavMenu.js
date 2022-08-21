e2ba4dc026a99ddc3556e7617811ce449d973330
Jing Wen <jingwen9@gmail.com>
2022-08-21 11:57:17 +1000

    Styling adjustments

--- src/NavMenu.js
+++ src/NavMenu.js
@@ -1,26 +1,27 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 export default ({ close }) => (
+
   <div className="menu">
     <ul>
       <li className="menu-list">
-        <Link to="/">
-            <p className="menu-code">Chapter .01</p>
-            <p className="menu-title">Home</p>
-        </Link>
+      <NavLink to="/" activeClassName="active">
+          <p className="menu-code">Chapter .01</p>
+          <p className="menu-title">Home</p>
+      </NavLink>
       </li>
       <li className="menu-list">
-        <Link to="/projects">
+        <NavLink to="/projects" activeClassName="active">
           <p className="menu-code">Chapter .02</p>
           <p className="menu-title">Projects</p>
-        </Link>
+        </NavLink>
       </li>
       <li className="menu-list">
-        <Link to="/about">
+        <NavLink to="/about">
             <p className="menu-code">Chapter .03</p>
             <p className="menu-title">About Me</p>
-        </Link>
+        </NavLink>
       </li>
       <li className="menu-list">
         <p className="menu-code">Chapter .04</p>
