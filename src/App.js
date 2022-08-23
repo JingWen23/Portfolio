5accd0aa5d4f3949de668d42eac5f9541ec58549
Jing Wen <jingwen9@gmail.com>
2022-08-23 14:04:44 +1000

    test edits for deployment

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={ <Home/> } />
+          <Route path="/portfolio" element={ <Home/> } />
           <Route path="/about" element={ <About/> } />
           <Route path="/projects" element={ <Projects/> } />
           <Route path="/project/1" element={ <ProjectOne/> } />
